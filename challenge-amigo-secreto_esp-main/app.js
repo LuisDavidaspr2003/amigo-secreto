40b76bc02ba42b8d6f7635f705d660daef2de7de
LuisDavidaspr2003 <luisdavidasprilla24@gmail.com>
2025-09-02 16:53:17 -0500

    actualizacion

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,3 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let guardarAmigos = [];
 
+function agregarAmigo() {
+  let añado = document.getElementById('amigo').value.trim(); // quitamos espacios extra
 
+  if (añado === '') {
+    alert('Escribe un nombre');
+  } else {
+    guardarAmigos.push(añado);
+    console.log(guardarAmigos);
+    limpiarCaja();
+  }
+}
+
+function limpiarCaja() {
+  document.getElementById('amigo').value = '';
+}
+
+function sortearAmigo() {
+  if (guardarAmigos.length === 0) {
+    alert('No has ingresado ningún nombre');
+  } else {
+    // Generar índice aleatorio
+    let indiceAleatorio = Math.floor(Math.random() * guardarAmigos.length);
+
+    // Obtener el nombre sorteado
+    let nombreSorteado = guardarAmigos[indiceAleatorio];
+
+    // Mostrar resultado en la página
+    document.getElementById('resultado').innerHTML = 
+      `🎉 El amigo secreto es: <b> ${nombreSorteado}</b>`;
+
+    console.log("Amigo secreto:", nombreSorteado);
+  }
+}
